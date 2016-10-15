fd184eb195fbe52d43ad79dbfb6ee1bbe516017c
Fayadh A <fayadh56@gmail.com>
2016-10-15 19:45:41 -0400

    nothing

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fayadh.vocal_finance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // hello
+    }
+}
